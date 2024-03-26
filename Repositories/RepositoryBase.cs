a5c79b8053545dbd2f7f516c017e0cc973af3176
DAGWebs <38997647+DAGWebs@users.noreply.github.com>
2024-03-25 20:04:32 -0500

    Add project files.

---
+++ Repositories/RepositoryBase.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace Dashboard.Repositories
+{
+    public abstract class RepositoryBase
+    {
+        private readonly string _connectionString;
+
+        public RepositoryBase()
+        {
+            _connectionString = "Server=(local); Database=MVVMLoginDb; Integrated Security=true; TrustServerCertificate=true";
+        }
+
+        protected SqlConnection GetConnection()
+        {
+            try
+            {
+                SqlConnection connection = new SqlConnection(_connectionString);
+                connection.Open();
+                connection?.Close();
+                return connection;
+            }
+            catch (Exception ex)
+            {
+                // Log the exception
+                Console.WriteLine($"An error occurred while establishing database connection: {ex.Message}");
+                throw; // Re-throw the exception to let the caller know there was an error
+            }
+        }
+    }
+}
